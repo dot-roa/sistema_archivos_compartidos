f3e37d5f8b49c48c02e79632349554b18c3e2aa3
marodriguezn <marodriguezn@uqvirtual.com>
2020-10-13 19:26:37 -0500

    anexo de las clases peer

---
+++ src/Peer/PeerRegisterProtocol.java
@@ -0,0 +1,29 @@
+package Peer;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PeerRegisterProtocol {
+
+	public static String protocol(ObjectOutputStream output, ObjectInputStream input, ArrayList<String> files)
+			throws IOException, ClassNotFoundException {
+		
+		System.out.print("Ingrese su nombre para ser registrado en el server: ");
+		String message = new Scanner(System.in).nextLine();
+		output.writeObject(message);
+		output.flush();
+		Object obj = (String) input.readObject();
+		System.out.println("Respuesta del server: " + obj);
+		System.out.println("Enviando Archivos ...");
+		output.writeObject(files);
+		output.flush();
+		obj = (String) input.readObject();
+		System.out.println("Respuesta del server: " + obj);
+		return "3400";
+
+	}
+
+}
