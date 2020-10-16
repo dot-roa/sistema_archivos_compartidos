4c50fb45c4ea4314048c6ecc8eb085f208164e2c
marodriguezn <marodriguezn@uqvirtual.com>
2020-10-15 19:41:43 -0500

    Codigo de recorrido de carpeta

--- src/Peer/PeerRegisterProtocol.java
+++ src/Peer/PeerRegisterProtocol.java
@@ -1,8 +1,13 @@
 package Peer;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -11,6 +16,7 @@ public class PeerRegisterProtocol {
 	public static String protocol(ObjectOutputStream output, ObjectInputStream input, ArrayList<String> files)
 			throws IOException, ClassNotFoundException {
 		
+		String rutaCarpeta = "C:\\Users\\migue\\Downloads\\torrent pc";
 		System.out.print("Ingrese su nombre para ser registrado en el server: ");
 		String message = new Scanner(System.in).nextLine();
 		output.writeObject(message);
@@ -18,6 +24,20 @@ public class PeerRegisterProtocol {
 		Object obj = (String) input.readObject();
 		System.out.println("Respuesta del server: " + obj);
 		System.out.println("Enviando Archivos ...");
+		
+		//////////////
+
+		try(DirectoryStream<Path> ds = Files.newDirectoryStream(Paths.get(rutaCarpeta))) {
+			
+			for(Path ruta : ds) {
+				System.out.println(ruta.getFileName());
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println(e.getStackTrace());
+		}
+		
+		////////////////
 		output.writeObject(files);
 		output.flush();
 		obj = (String) input.readObject();
